2463c80e0c5bb300c0f701195863ed28c52af01b
Stevo John <stevehuwjohn@gmail.com>
2025-04-28 13:20:05 +0100

    Starting solver

--- src/TrainTracks.Engine/Solver.cs
+++ src/TrainTracks.Engine/Solver.cs
@@ -4,7 +4,14 @@ namespace TrainTracks.Engine;
 
 public class Solver
 {
+    private Grid _grid;
+    
+    private Point _position;
+    
     public void Solve(Grid grid)
     {
+        _grid = grid;
+
+        _position = _grid.Entry;
     }
 }
