837c9716c5968d09d9f1a2ce8ffc76a85ecbb4cf
Stevo John <stevehuwjohn@gmail.com>
2025-04-30 15:16:58 +0100

    Code cleanup

--- src/TrainTracks.Engine/Board/Connector.cs
+++ src/TrainTracks.Engine/Board/Connector.cs
@@ -3,7 +3,7 @@ namespace TrainTracks.Engine.Board;
 public static class Connector
 {
     private static readonly List<Piece> _connections = [];
-    
+
     public static readonly List<(int Dx, int Dy)> Directions =
     [
         (0, -1),
@@ -11,102 +11,112 @@ public static class Connector
         (0, 1),
         (-1, 0)
     ];
-    
+
     public static IReadOnlyList<Piece> GetConnections(Piece piece, int dX, int dY)
     {
         _connections.Clear();
 
+        switch (piece)
+        {
+            case Piece.Horizontal:
+                switch (dX, dY)
+                {
+                    case (1, 0):
+                        _connections.Add(Piece.Horizontal);
+                        _connections.Add(Piece.NorthWest);
+                        _connections.Add(Piece.SouthWest);
+                        break;
+                    case (-1, 0):
+                        _connections.Add(Piece.Horizontal);
+                        _connections.Add(Piece.NorthEast);
+                        _connections.Add(Piece.SouthEast);
+                        break;
+                }
 
-    switch (piece)
-    {
-        case Piece.Horizontal:
-            switch (dX, dY)
-            {
-                case (1, 0):
-                    _connections.Add(Piece.Horizontal);
-                    _connections.Add(Piece.NorthWest);
-                    _connections.Add(Piece.SouthWest);
-                    break;
-                case (-1, 0):
-                    _connections.Add(Piece.Horizontal);
-                    _connections.Add(Piece.NorthEast);
-                    _connections.Add(Piece.SouthEast);
-                    break;
-            }
-            break;
+                break;
 
-        case Piece.Vertical:
-            switch (dX, dY)
-            {
-                case (0, -1):
-                    _connections.Add(Piece.Vertical);
-                    _connections.Add(Piece.SouthEast);
-                    _connections.Add(Piece.SouthWest);
-                    break;
-                case (0, 1):
-                    _connections.Add(Piece.Vertical);
-                    _connections.Add(Piece.NorthEast);
-                    _connections.Add(Piece.NorthWest);
-                    break;
-            }
-            break;
+            case Piece.Vertical:
+                switch (dX, dY)
+                {
+                    case (0, -1):
+                        _connections.Add(Piece.Vertical);
+                        _connections.Add(Piece.SouthEast);
+                        _connections.Add(Piece.SouthWest);
+                        break;
+                    case (0, 1):
+                        _connections.Add(Piece.Vertical);
+                        _connections.Add(Piece.NorthEast);
+                        _connections.Add(Piece.NorthWest);
+                        break;
+                }
 
-        case Piece.NorthEast:
-            switch (dX, dY)
-            {
-                case (0, 1): 
-                    _connections.Add(Piece.Horizontal);
-                    _connections.Add(Piece.SouthEast);
-                    break;
-                case (-1, 0):
-                    _connections.Add(Piece.Vertical);
-                    _connections.Add(Piece.NorthWest);
-                    break;
-            }
-            break;
+                break;
 
-        case Piece.SouthEast:
-            switch (dX, dY)
-            {
-                case (0, -1):
-                    _connections.Add(Piece.Horizontal);
-                    _connections.Add(Piece.NorthEast);
-                    break;
-                case (-1, 0):
-                    _connections.Add(Piece.Vertical);
-                    _connections.Add(Piece.SouthWest);
-                    break;
-            }
-            break;
+            case Piece.NorthEast:
+                switch (dX, dY)
+                {
+                    case (0, 1):
+                        _connections.Add(Piece.Horizontal);
+                        _connections.Add(Piece.SouthEast);
+                        break;
+                    case (-1, 0):
+                        _connections.Add(Piece.Vertical);
+                        _connections.Add(Piece.NorthWest);
+                        break;
+                }
 
-        case Piece.SouthWest:
-            switch (dX, dY)
-            {
-                case (0, -1):
-                    _connections.Add(Piece.Horizontal);
-                    _connections.Add(Piece.NorthWest);
-                    break;
-                case (1, 0):
-                    _connections.Add(Piece.Vertical);
-                    _connections.Add(Piece.SouthEast);
-                    break;
-            }
-            break;
+                break;
 
-        case Piece.NorthWest:
-            switch (dX, dY)
-            {
-                case (0, 1):
-                    _connections.Add(Piece.Horizontal);
-                    _connections.Add(Piece.SouthWest);
-                    break;
-                case (1, 0):
-                    _connections.Add(Piece.Vertical);
-                    _connections.Add(Piece.NorthEast);
-                    break;
-            }
-            break;
-    }        
+            case Piece.SouthEast:
+                switch (dX, dY)
+                {
+                    case (0, -1):
+                        _connections.Add(Piece.Horizontal);
+                        _connections.Add(Piece.NorthEast);
+                        break;
+                    case (-1, 0):
+                        _connections.Add(Piece.Vertical);
+                        _connections.Add(Piece.SouthWest);
+                        break;
+                }
+
+                break;
+
+            case Piece.SouthWest:
+                switch (dX, dY)
+                {
+                    case (0, -1):
+                        _connections.Add(Piece.Horizontal);
+                        _connections.Add(Piece.NorthWest);
+                        break;
+                    case (1, 0):
+                        _connections.Add(Piece.Vertical);
+                        _connections.Add(Piece.SouthEast);
+                        break;
+                }
+
+                break;
+
+            case Piece.NorthWest:
+                switch (dX, dY)
+                {
+                    case (0, 1):
+                        _connections.Add(Piece.Horizontal);
+                        _connections.Add(Piece.SouthWest);
+                        break;
+                    case (1, 0):
+                        _connections.Add(Piece.Vertical);
+                        _connections.Add(Piece.NorthEast);
+                        break;
+                }
+
+                break;
+
+            case Piece.Empty:
+            default:
+                throw new ArgumentOutOfRangeException(nameof(piece), piece, null);
+        }
+
         return _connections;
     }
 }
