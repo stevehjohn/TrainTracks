797c05898b5915fb7415a6fce27af725dbd8633c
Stevo John <stevehuwjohn@gmail.com>
2025-05-03 14:40:33 +0100

    WIP

--- src/TrainTracks.Console/EntryPoint.cs
+++ src/TrainTracks.Console/EntryPoint.cs
@@ -18,7 +18,7 @@ public static class EntryPoint
 
         PuzzleManager.Path = "Data/puzzles.json";
         
-        var puzzle = PuzzleManager.Instance.Puzzles[8];
+        var puzzle = PuzzleManager.Instance.Puzzles[2];
 
         Clear();
         
