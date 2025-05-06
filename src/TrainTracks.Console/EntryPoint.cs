3579a6bfa17fb5cbb1fa097c12af6dddba7ed199
Stevo John <stevehuwjohn@gmail.com>
2025-05-06 10:35:44 +0100

    WIP

--- src/TrainTracks.Console/EntryPoint.cs
+++ src/TrainTracks.Console/EntryPoint.cs
@@ -4,10 +4,6 @@ namespace TrainTracks.Console;
 
 public static class EntryPoint
 {
-    private static long _count;
-
-    private static Stopwatch _stopwatch;
-    
     public static void Main(string[] arguments)
     {
 
