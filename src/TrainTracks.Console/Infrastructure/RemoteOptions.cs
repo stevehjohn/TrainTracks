12f0d23145bbaf9b6b352d5ede4babab8ebc7d4b
Stevo John <stevehuwjohn@gmail.com>
2025-05-06 10:47:07 +0100

    WIP

--- src/TrainTracks.Console/Infrastructure/RemoteOptions.cs
+++ src/TrainTracks.Console/Infrastructure/RemoteOptions.cs
@@ -1,5 +1,10 @@
+using CommandLine;
+using JetBrains.Annotations;
+
 namespace TrainTracks.Console.Infrastructure;
 
+[UsedImplicitly]
+[Verb("remote", HelpText = "Run a puzzle from Puzzle Madness.")]
 public class RemoteOptions
 {
     
