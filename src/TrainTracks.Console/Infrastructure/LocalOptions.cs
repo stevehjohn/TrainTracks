04e5eb04b6c92d3baccc44fa60fda82e7cac87aa
Stevo John <stevehuwjohn@gmail.com>
2025-05-06 10:48:33 +0100

    WIP

--- src/TrainTracks.Console/Infrastructure/LocalOptions.cs
+++ src/TrainTracks.Console/Infrastructure/LocalOptions.cs
@@ -7,5 +7,6 @@ namespace TrainTracks.Console.Infrastructure;
 [Verb("local", HelpText = "Run a puzzle from the local file system.")]
 public class LocalOptions
 {
+    [Option('n', "number", Required = true, HelpText = "The puzzle number to run.")]
     public int PuzzleNumber { get; set; }
 }
