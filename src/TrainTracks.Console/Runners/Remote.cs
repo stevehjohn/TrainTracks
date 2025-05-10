0bba8d680cfc791a49d6c58924eaaac54bba2e58
Stevo John <stevehuwjohn@gmail.com>
2025-05-10 15:30:03 +0100

    Rate limiting

--- src/TrainTracks.Console/Runners/Remote.cs
+++ src/TrainTracks.Console/Runners/Remote.cs
@@ -89,8 +89,12 @@ public class Remote
 
             WriteLine();
 
+            Thread.Sleep(1_000);
+
             var statusCode = client.SendResult(puzzle.Value.Date, puzzle.Value.Grid, puzzle.Value.Variant);
 
+            Thread.Sleep(1_000);
+            
             if (statusCode != HttpStatusCode.OK)
             {
                 WriteLine($"Result not accepted. Status code: {(int) statusCode}.");
