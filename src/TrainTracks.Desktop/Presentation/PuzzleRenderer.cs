f7fa9bdebaa67a1899f0e33968615d934f1b58f6
Stevo John <stevehuwjohn@gmail.com>
2025-05-18 09:19:22 +0100

    WIP

--- src/TrainTracks.Desktop/Presentation/PuzzleRenderer.cs
+++ src/TrainTracks.Desktop/Presentation/PuzzleRenderer.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using TrainTracks.Desktop.Infrastructure;
 using TrainTracks.Engine;
 using TrainTracks.Engine.Board;
@@ -12,16 +13,24 @@ public class PuzzleRenderer : Game
     // ReSharper disable once NotAccessedField.Local
     private GraphicsDeviceManager _graphics;
 
-    private Solver _solve = new();
+    private SpriteBatch _spriteBatch;
+
+    private readonly int _width;
+    
+    private Solver _solver = new();
     
     public Grid Grid { get; set; }
     
     public PuzzleRenderer()
     {
+        _width = Constants.PuzzleMaxWidth * Constants.TileWidth;
+
+        _height = Constants.PuzzleMaxHeight * Constants.TileHeight;
+            
         _graphics = new GraphicsDeviceManager(this)
         {
-            PreferredBackBufferWidth = Constants.PuzzleMaxWidth * Constants.TileWidth,
-            PreferredBackBufferHeight = Constants.PuzzleMaxHeight * Constants.TileHeight
+            PreferredBackBufferWidth = _width,
+            PreferredBackBufferHeight = _height
         };
 
         Content.RootDirectory = "_Content";
@@ -31,6 +40,8 @@ public class PuzzleRenderer : Game
 
     protected override void LoadContent()
     {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
         _tileMapper.LoadContent(Content);
     }
 
@@ -43,6 +54,27 @@ public class PuzzleRenderer : Game
     {
         GraphicsDevice.Clear(Color.Black);
 
+        _spriteBatch.Begin(SpriteSortMode.FrontToBack);
+        
+        for (var x = 0; x < Grid.Width; x++)
+        {
+            for (var y = 0; y < Grid.Height; y++)
+            {
+                if (Grid[x, y] is Piece.Empty or Piece.Cross or Piece.Placeholder)
+                {
+                    continue;
+                }
+
+                var isometricX = (x - y) * Constants.TileWidth / 2 + _width / 2;
+                
+                var isometricY = (x + y) * Constants.TileHeight / 2;
+                
+                _spriteBatch.Draw(_tileMapper.GetTile(Grid[x, y]), new Vector2(isometricX, isometricY), Color.White);;
+            }
+        }
+
+        _spriteBatch.End();
+        
         base.Draw(gameTime);
     }
 }
