50251a10e049821c20c6ff259c414c6c7bfae5ed
Stevo John <stevehuwjohn@gmail.com>
2026-04-29 14:28:28 +0100

    Fixing client?

--- src/TrainTracks.Engine/Infrastructure/PuzzleClient.cs
+++ src/TrainTracks.Engine/Infrastructure/PuzzleClient.cs
@@ -17,6 +17,8 @@ public sealed class PuzzleClient : IDisposable
 
     private readonly int _userId;
 
+    private int _latestYear = 2005;
+
     private readonly JsonSerializerOptions _jsonSerializerOptions = new()
     {
         PropertyNameCaseInsensitive = true
@@ -28,7 +30,8 @@ public sealed class PuzzleClient : IDisposable
         
         _handler = new HttpClientHandler
         {
-            CookieContainer = cookieContainer
+            CookieContainer = cookieContainer,
+            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate | DecompressionMethods.Brotli
         };
 
         var lines = File.ReadAllLines("cookies.txt");
@@ -47,10 +50,18 @@ public sealed class PuzzleClient : IDisposable
         
         _client = new HttpClient(_handler)
         {
-            BaseAddress = new Uri(BaseUri)
+            BaseAddress = new Uri(BaseUri),
+            Timeout = TimeSpan.FromSeconds(30)
         };
-        
-        _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.166 Safari/537.36");
+
+        _client.DefaultRequestHeaders.Add("User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0");
+
+        _client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+
+        _client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+
+        _client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9");
     }
     
     public (DateOnly Date, Grid Grid, int Variant)? GetNextPuzzle(Difficulty difficulty)
@@ -83,6 +94,41 @@ public sealed class PuzzleClient : IDisposable
         return (nextPuzzleDate.Value, new Grid(puzzle), puzzle.Source.Variant);
     }
 
+    private DateOnly? GetOldestIncompletePuzzleDate(Difficulty difficulty)
+    {
+        var now = DateTime.Now;
+
+        for (var year = _latestYear; year <= now.Year; year++)
+        {
+            using var response = _client.GetAsync($"/archive/traintracks/{difficulty.ToString().ToLower()}/{year}").Result;
+            
+            var page = response.Content.ReadAsStringAsync().Result;
+
+            var dom = new HtmlDocument();
+            
+            dom.LoadHtml(page);
+            
+            var puzzles = dom.DocumentNode.SelectNodes("//td[@class='puzzleNotDone']");
+
+            if (puzzles != null && puzzles.Count > 0)
+            {
+                var puzzle = puzzles[0];
+                
+                var id =puzzle.Attributes["id"].Value;
+
+                var parts = id.Split('-');
+                
+                _latestYear = year - 1;
+
+                return new DateOnly(int.Parse(parts[3]), int.Parse(parts[4]), int.Parse(parts[5]));
+            }
+            
+            Thread.Sleep(TimeSpan.FromMilliseconds(3_000));
+        }
+
+        return null;   
+    }
+
     public (HttpStatusCode StatusCode, PuzzleSolvedResponse Response) SendResult(DateOnly date, Grid grid, int variant)
     {
         Thread.Sleep(TimeSpan.FromMilliseconds(1_000));
@@ -110,7 +156,7 @@ public sealed class PuzzleClient : IDisposable
 
         score -= grid.FixedPieceCount * 5;
 
-        var solution = new PuzzleSolution
+        var payload = new PuzzleSolution
         {
             Type = 33,
             Variant = variant,
@@ -124,48 +170,15 @@ public sealed class PuzzleClient : IDisposable
             CreatedAt = timestamp
         };
 
-        var content = JsonSerializer.Serialize(solution);
-        
-        var stringContent = new StringContent(content);
-        
-        using var responseObject = _client.PostAsync("user/puzzlecomplete", stringContent).Result;
-        
+        var json = JsonSerializer.Serialize(payload, _jsonSerializerOptions);
+
+        using var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        using var responseObject = _client.PostAsync("user/puzzlecomplete", content).Result;
+
         var response = JsonSerializer.Deserialize<PuzzleSolvedResponse>(responseObject.Content.ReadAsStringAsync().Result, _jsonSerializerOptions);
 
         return (responseObject.StatusCode, response);
-    }
-
-    private DateOnly? GetOldestIncompletePuzzleDate(Difficulty difficulty)
-    {
-        var now = DateTime.Now;
-
-        for (var year = 2005; year <= now.Year; year++)
-        {
-            using var response = _client.GetAsync($"/archive/traintracks/{difficulty.ToString().ToLower()}/{year}").Result;
-            
-            var page = response.Content.ReadAsStringAsync().Result;
-
-            var dom = new HtmlDocument();
-            
-            dom.LoadHtml(page);
-            
-            var puzzles = dom.DocumentNode.SelectNodes("//td[@class='puzzleNotDone']");
-
-            if (puzzles != null && puzzles.Count > 0)
-            {
-                var puzzle = puzzles[0];
-                
-                var id =puzzle.Attributes["id"].Value;
-
-                var parts = id.Split('-');
-                
-                return new DateOnly(int.Parse(parts[3]), int.Parse(parts[4]), int.Parse(parts[5]));
-            }
-        }
-            
-        Thread.Sleep(TimeSpan.FromMilliseconds(3_000));
-
-        return null;   
     }
 
     public void Dispose()
